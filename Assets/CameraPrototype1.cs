6a7faa5ea0fd2b552b39c1da5bbeb809d4b9e4e7
Alexander <aradostev@softaria.com>
2016-12-06 18:22:32 +0700

    derp

---
+++ Assets/CameraPrototype1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPrototype1 : MonoBehaviour {
+
+    public CharacterPrototype1 ch;
+    public float distance = 2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/CharacterPrototype1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterPrototype1 : MonoBehaviour {
+
+    protected Rigidbody rb;
+    public Camera cam;
+
+    public GvrReticle camReticle;
+    public float speed = 1f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        if (null == rb)
+            rb = GetComponentInChildren<Rigidbody>();
+    }
+    // Update is called once per frame
+ 
+	void Update () {
+		if (camReticle.Gazed)
+        {
+            RaycastHit hitInfo;
+            Physics.Raycast(cam.transform.position, cam.transform.forward - cam.transform.position, out hitInfo);
+
+            Debug.DrawLine(cam.transform.position, hitInfo.point, Color.red, 1f);
+            //rb.MovePosition(Vector3.Lerp(rb.transform.position, camReticle.transform.position - rb.transform.position, speed * Time.deltaTime));
+        }
+	}
+}
