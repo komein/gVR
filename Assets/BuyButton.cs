18ef49e950cc4c0673f89b0ce2e769798849191c
Alexander <aradostev@softaria.com>
2017-02-01 20:55:59 +0700

    small visibility fix for buy button

--- Assets/BuyButton.cs
+++ Assets/BuyButton.cs
@@ -29,7 +29,7 @@ public class BuyButton : SceneButton {
     {
         if (null != container)
         {
-            container.enabled = v;
+            container.gameObject.SetActive(v);
         }
     }
 }
