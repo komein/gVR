a9e733d166d276429939eb27c08a3c487e473af8
Alexander <aradostev@softaria.com>
2017-02-01 21:01:20 +0700

    repositioning the interface another visibility fix for buy button

--- Assets/BuyButton.cs
+++ Assets/BuyButton.cs
@@ -9,13 +9,17 @@ public class BuyButton : SceneButton {
     DataStorage store;
     public LevelButtonContainer container;
 
+    private void Awake()
+    {
+        Toggle(false);
+    }
+
     protected override void Start()
     {
         base.Start();
 
         store = FindObjectOfType<DataStorage>();
 
-        Toggle(false);
     }
 
     protected override void Function()
